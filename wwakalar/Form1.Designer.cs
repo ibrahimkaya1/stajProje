92e636f7be454a3163a6cef6041e95633ec39727
OsmanliDelisi <rockabraham5561@gmail.com>
2018-07-26 19:24:14 +0300

    V1.0
    26.07.2018

---
+++ wwakalar/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace wwakalar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(370, 225);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Başla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(370, 276);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "New Api_Key";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(370, 323);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "Göster";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1104, 606);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ wwakalar/Form1.cs
@@ -0,0 +1,256 @@
+﻿
+using System;
+
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+using System.Net;
+using System.IO;
+
+namespace wwakalar
+{
+    public partial class Form1 : Form
+    {
+        
+        //https://wakatime.com/api/v1/users/current/durations?date=2018-07-13&api_key=5fff4da8-ee27-43e6-9511-903fd88be248
+        public Form1()
+        {
+            
+            InitializeComponent();
+
+        }
+
+        
+        private string get_data(string url)
+        {
+
+            string urlAddress = url;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(urlAddress);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                Stream receiveStream = response.GetResponseStream();
+                StreamReader readStream = null;
+
+
+                readStream = new StreamReader(receiveStream);
+
+
+                string data = readStream.ReadToEnd();
+              //  var cek = JsonConvert.DeserializeObject<Baslik>(data);
+              //  listView1.Text = cek.Data.ToString();
+                response.Close();
+                readStream.Close();
+               
+                return data;
+
+            }
+            else
+                return "Deger yok";
+        }
+        string[] durat = new string[100];
+        string[] tm = new string[100];
+        string[] prject = new string[100];
+        string[] arrray_value = new string[100];
+        string api_key = "5fff4da8-ee27-43e6-9511-903fd88be248";
+        //string api_key = "084fb563-1f53-44d0-9ff2-48f2c51d202c";
+        string tarih = DateTime.Now.ToString();
+
+        string[] created = new string[100];
+        string[] entity = new string[100];
+        string[] time = new string[100];
+        string[] type = new string[100];
+
+        string veri_duration;
+        string veri_heartbeats;
+        string veri_user;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+             veri_duration = get_data("https://wakatime.com/api/v1/users/current/durations?date=2018-07-24&api_key="+api_key);
+            veri_heartbeats = get_data("https://wakatime.com/api/v1/users/current/heartbeats?date=2018-07-24&api_key=" + api_key);
+            veri_user = get_data("https://wakatime.com/api/v1/users/current?api_key=" + api_key);
+        }
+        /*{
+  "data": {
+    "created_at": "2018-06-25T05:48:32Z",+
+    "display_name": "Anonymous User",
+    "email": "ibrahimkaya334298@gmail.com",+
+    "email_public": false,
+    "full_name": null,
+    "has_premium_features": false,
+    "human_readable_website": null,
+    "id": "52cb9b43-fa90-4617-9c55-6e300ffb9c8f",+
+    "is_email_confirmed": true,
+    "is_hireable": false,
+    "languages_used_public": false,
+    "last_heartbeat": "2018-07-25T08:40:07Z",+
+    "last_plugin": "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.0.1594 Safari/537.36 chrome-wakatime/1.0.2",
+    "last_plugin_name": "Chrome",+
+    "last_project": "wwakalar",+
+    "location": null,
+    "logged_time_public": false,
+    "modified_at": null,
+    "photo": "https://wakatime.com/gravatar/52cb9b43-fa90-4617-9c55-6e300ffb9c8f",
+    "photo_public": true,
+    "plan": "basic",
+    "timezone": "Europe/Istanbul",
+    "username": null,
+    "website": null
+  }
+}*/
+        public string parse_json3(string json)
+        {
+            string value = string.Empty;
+
+            JObject json_object = JObject.Parse(json);
+            var json_result = json_object["data"];
+
+         
+                int i = 0;
+
+                string cretad = json_result["created_at"].ToString();
+
+                string mail = json_result["email"].ToString();
+               
+                string ids = json_result["id"].ToString();
+
+                string last_heartbeat = json_result["last_heartbeat"].ToString();
+                
+                string last_plugin_name = json_result["last_plugin_name"].ToString();
+
+                string last_project = json_result["last_project"].ToString();
+
+                string photo= json_result["photo"].ToString();
+
+                string timezone = json_result["timezone"].ToString();
+                value = "created_at=" + cretad + "\n" + "email=" + mail + "\n" + "id=" + ids + "\n" + "last_heartbeat=" 
+                    + last_heartbeat + "\n" + "last_plugin_name=" + last_plugin_name + "\n"+ "last_project="+ last_project+ "\n"+
+                    "photo="+ photo+"\n"+ "timezone="+ timezone+"\n";
+
+
+                /*****************************************************************************************/
+
+
+
+                Console.WriteLine(value); //burda ki çıktılar veritabanına kaydedilecek
+
+
+                /*****************************************************************************************/
+                //dizi_al(duration, i, time, project);
+            
+            return value;
+        }
+        /*
+         "created_at": "2018-07-13T07:17:59Z",
+      "entity": "chrome://newtab",
+      "id": "a8867564-09f6-47dc-8144-902c1a4daedc",
+      "time": 1531466283,
+      "type": "domain"
+             
+             */
+        public string parse_json2(string json)
+        {
+            string value = string.Empty;
+
+            JObject json_object = JObject.Parse(json);
+            var json_result = json_object["data"];
+
+            foreach (var datas in json_result)
+            {
+                int i = 0;
+
+                string cretad = datas["created_at"].ToString();
+                //  Console.WriteLine(duration);
+                created[i] = cretad.ToString();
+
+                string entit = datas["entity"].ToString();
+                entity[i] = entit.ToString();
+
+                string ids = datas["user_id"].ToString();
+               
+
+                string times = datas["time"].ToString();
+                time[i] = times.ToString();
+
+                string types  = datas["type"].ToString();
+                type[i] = types.ToString();
+
+                value = "created_at=" + cretad + "\n" + "entity=" + entit + "\n" + "id="+ids+"\n"+"time=" + times + "\n" +"type="+ types + "\n";
+
+
+                /*****************************************************************************************/
+
+
+
+                Console.WriteLine(value); //burda ki çıktılar veritabanına kaydedilecek
+              
+                
+                /*****************************************************************************************/
+                                          //dizi_al(duration, i, time, project);
+            }
+            return value;
+        }
+        public string parse_json(string json)
+        {
+            string value = string.Empty;
+
+            JObject json_object = JObject.Parse(json);
+            var json_result = json_object["data"];
+
+           
+            foreach (var datas in json_result)
+            {
+                int i = 0;
+                   
+                string duration = datas["duration"].ToString();
+              //  Console.WriteLine(duration);
+                durat[i]=duration.ToString();
+               
+                string project = datas["project"].ToString();
+                prject[i] = project.ToString();
+                string time = datas["time"].ToString();
+                tm[i] = time.ToString();
+                var end = datas["end"];
+                var start = datas["start"];
+                var timezone = datas["timezone"];
+
+                value = "duration=" + duration + "\n" + "project=" + project + "\n" + "time=" + time + "\n"+timezone+"\n";
+
+
+
+                /*****************************************************************************************/
+
+
+                Console.WriteLine(value);//burdaki veriler  ayrı tabloda tutulacak
+                                      
+                
+                /*****************************************************************************************/
+
+
+
+
+              //  dizi_al(duration,i,time,project);
+            }
+            return value;
+        }
+        /*public void dizi_al(string duration,int i, string time,string project)
+        {
+           
+            durat[i] = duration;
+            tm[i] = time;
+            prject[i] = project;
+           Console.WriteLine(i+") "+durat[i]+" "+tm[i]+" "+prject[i]);
+        }*/
+        private void button1_Click(object sender, EventArgs e)
+        {
+            parse_json2(veri_heartbeats);
+            Console.WriteLine("*************************************************************************************");
+            parse_json(veri_duration);
+            Console.WriteLine("*************************************************************************************");
+            parse_json3(veri_user);
+            //  var val = "duration=" + durat[2] + "\n" + "project=" + prject[2] + "\n" + "time=" + tm[2];
+            //   richTextBox1.Text = val.ToString();
+        }
+    }
+}
